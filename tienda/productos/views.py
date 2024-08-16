6a4cafaceb544858fc17048dda5ee5e682a3602e
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-08-15 20:43:31 -0500

    paginacion

--- tienda/productos/views.py
+++ tienda/productos/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Cliente, Producto
 from .forms import ProductoModelForm, ClienteModelForm
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Create your views here.
 def listar_clientes(request): 
@@ -9,16 +10,20 @@ def listar_clientes(request):
     return render(request, 'listar_clientes.html', {'clientes': clientes})
 
 def listar_productos(request): 
-    productos = Producto.objects.all()
+    productos_list = Producto.objects.all()
     precio_min = request.GET.get('precio_min')
     precio_max = request.GET.get('precio_max')
     query = request.GET.get('q')
+    paginator = Paginator(productos_list, 10)
+
+    page_number = request.GET.get('page')
+    productos= paginator.get_page('page_number')
 
     if precio_min and precio_max:
-        productos = productos.filter(precio__gte = precio_min, precio__lte= precio_max)
+        productos_list = productos_list.filter(precio__gte = precio_min, precio__lte= precio_max)
         
     if query:
-        productos = productos.filter(Q(nombre__icontains = query)| Q(descripcion__icontains = query))
+        productos_list = productos_list.filter(Q(nombre__icontains = query)| Q(descripcion__icontains = query))
     return render(request, 'listar_productos.html', {'productos': productos})
 
 def agregar_producto(request): 
