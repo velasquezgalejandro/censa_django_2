5f762830ddf00132fb1de48bbe9defb4d924f559
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-09-03 20:24:03 -0500

    commit clase 3

--- Poo_avanzado/gestion_inventarios/inventario/forms.py
+++ Poo_avanzado/gestion_inventarios/inventario/forms.py
@@ -1,18 +1,41 @@
 from django import forms
-from .models import Producto, Categoria, Proveedor
+from .models import Producto, Categoria, Proveedor, DetalleProducto
 
 class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
         fields = ['nombre', 'categoria', 'proveedor', 'etiquetas', 'cantidad', 'precio', 'descripcion']
 
+    def clena_precio(self):
+
+        precio = self.cleaned_data.get('precio')
+        if precio < 0:
+            raise forms.ValidationError('El precio no pude ser negativo')
+        return precio
 
 class CategoriaForm(forms.ModelForm):
     class Meta:
         model = Categoria
         fields = ['nombre', 'descripcion']
 
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        if not nombre: 
+            raise forms.ValidationError('El nombre no pude quedar vacio')
+        return nombre
+
 class ProveedorForm(forms.ModelForm):
     class Meta:
         model = Proveedor
         fields = ['nombre', 'contacto', 'telefono']
+
+    def clean_telefono(self):
+        telefono = self.cleaned_data.get('telefono')
+        if not telefono.isdigit():
+            raise forms.ValidationError('El numero de telefono debe contener solo digitos.')
+        return telefono
+
+class DetalleProductoForm(forms.ModelForm):
+    class Meta:
+        model = DetalleProducto
+        fields = ['espicificaciones', 'fecha_vencimiento']
--- Poo_avanzado/gestion_inventarios/inventario/views.py
+++ Poo_avanzado/gestion_inventarios/inventario/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
-from .models import Producto, Categoria, Proveedor
-from .forms import ProductoForm, CategoriaForm, ProveedorForm
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Producto, Categoria, Proveedor, DetalleProducto
+from .forms import ProductoForm, CategoriaForm, ProveedorForm, DetalleProductoForm
 
 # Create your views here.
 
@@ -14,17 +14,23 @@ def listar_productos(request):
 
 def agregar_producto(request):
     if request.method == 'POST':
-        form = ProductoForm(request.POST)
-        if form.is_valid():
-            form.save()
+        producto_form = ProductoForm(request.POST)
+        detalle_form =  DetalleProductoForm(request.POST)
+        if producto_form.is_valid() and detalle_form.is_valid():
+            producto = producto_form.save()
+            detalle = detalle_form.save(commit=False)
+            detalle.producto = producto
+            detalle.save()
             return redirect('listar_productos')
-    else:
-        form = ProductoForm()
-    return render(request, 'inventario/agregar_producto.html', {'form': form})
+    else: 
+        producto_form = ProductoForm()
+        detalle_form = DetalleProductoForm()
+    return render(request, 'inventario/agregar_producto.html', {'producto_form': producto_form , 'detalle_form': detalle_form})
 
+        
 
 def editar_producto(request, pk):
-    producto = Producto.objects.get(pk=pk)
+    producto = get_object_or_404(Producto, pk=pk)
     if request.method == 'POST':
         form = ProductoForm(request.POST, instance=producto)
         if form.is_valid():
@@ -35,17 +41,11 @@ def editar_producto(request, pk):
     return render(request, 'inventario/editar_producto.html', {'form': form})
 
 def eliminar_producto(request, pk):
-    producto = Producto.objects.get(pk=pk)
+    producto = get_object_or_404(Producto, pk=pk)
     if request.method == 'POST':
         producto.delete()
         return redirect('listar_producto')
     return render(request, 'inventario/eliminar_producto.html', {'producto': producto})
-
-
-
-
-
-# Vistas para Producto (ya están definidas)
 
 # Vistas para Categoria
 def listar_categorias(request):
@@ -60,10 +60,10 @@ def agregar_categoria(request):
             return redirect('listar_categorias')
     else:
         form = CategoriaForm()
-    return render(request, 'inventario/agregar_categoria.html', {'form': form})
+    return render(request, 'inventario/agregar_categorias.html', {'form': form})
 
 def editar_categoria(request, pk):
-    categoria = Categoria.objects.get(pk=pk)
+    categoria = get_object_or_404(Categoria, pk=pk)
     if request.method == 'POST':
         form = CategoriaForm(request.POST, instance=categoria)
         if form.is_valid():
@@ -74,7 +74,7 @@ def editar_categoria(request, pk):
     return render(request, 'inventario/editar_categoria.html', {'form': form})
 
 def eliminar_categoria(request, pk):
-    categoria = Categoria.objects.get(pk=pk)
+    categoria = get_object_or_404(Categoria, pk=pk)
     if request.method == 'POST':
         categoria.delete()
         return redirect('listar_categorias')
@@ -96,7 +96,7 @@ def agregar_proveedor(request):
     return render(request, 'inventario/agregar_proveedor.html', {'form': form})
 
 def editar_proveedor(request, pk):
-    proveedor = Proveedor.objects.get(pk=pk)
+    proveedor = get_object_or_404(Proveedor, pk=pk)
     if request.method == 'POST':
         form = ProveedorForm(request.POST, instance=proveedor)
         if form.is_valid():
@@ -107,8 +107,10 @@ def editar_proveedor(request, pk):
     return render(request, 'inventario/editar_proveedor.html', {'form': form})
 
 def eliminar_proveedor(request, pk):
-    proveedor = Proveedor.objects.get(pk=pk)
+    proveedor = get_object_or_404(Proveedor, pk=pk)
     if request.method == 'POST':
         proveedor.delete()
         return redirect('listar_proveedores')
     return render(request, 'inventario/eliminar_proveedor.html', {'proveedor': proveedor})
+
+
